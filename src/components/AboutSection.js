c7c0b9b4f98e43c61e593d3ef2e6d923a7fce1bd
Alok-Senapati <aloksenapati470@gmail.com>
2021-01-14 00:26:38 +0530

    AboutUs Section Content

---
+++ src/components/AboutSection.js
@@ -0,0 +1,32 @@
+import React from "react";
+import home1 from "../img/home1.png";
+
+function AboutSection() {
+  return (
+    <div>
+      <div className="description">
+        <div className="hide">
+          <h1>We work to make</h1>
+        </div>
+        <div className="hide">
+          <h1>
+            your <span>dreams</span> come
+          </h1>
+        </div>
+        <div className="hide">
+          <h1>true.</h1>
+        </div>
+        <p>
+          Contact us for any photography or videography ideas that you have. We
+          have proffesionals with amazing skills.
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <div className="image">
+        <img src={home1} alt="A guy with camera" />
+      </div>
+    </div>
+  );
+}
+
+export default AboutSection;
